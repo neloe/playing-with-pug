a58db5f55de366da2c8b06b7281ad070ca7e4efa
Nathan Eloe <powerofazure@gmail.com>
2020-04-06 10:45:17 -0500

    Add mongoose and sample model

---
+++ controllers/product.js
@@ -0,0 +1,6 @@
+const Product = require('../models/product')
+
+exports.count = function(req, res) {
+    // TODO: add logic for counting products
+    res.render('count', {})
+}
---
+++ models/product.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    name: {type: String, required: true},
+    price: {type: Number, default: 0.0}
+})
+
+module.exports = mongoose.model('Product', productSchema)
