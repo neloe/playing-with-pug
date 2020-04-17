41bfd7a1e00825d0f70d7593aac956ce820e0d4e
Nathan Eloe <powerofazure@gmail.com>
2020-04-17 10:07:40 -0500

    Add create controller and view for product

--- controllers/product.js
+++ controllers/product.js
@@ -12,5 +12,23 @@ exports.testview = function(req, res) {
     Product.findOne({name: 'product1'}, function(err, product) {
         console.log(product)
         res.render('product-read',{title: product.name, name: product.name, price: product.price})
+    })                         
+}
+
+exports.view_one = function(req, res) {
+    Product.findOne({name: req.params.productName}, function(err, product) {
+        if (product)
+            res.render('product-read',{title: product.name, name: product.name, price: product.price})
+        else
+            res.render('index', {title:'Error'})
+    })
+}
+
+exports.create = function(req, res) {
+    //console.log(req.body)
+    //res.render('index', {})
+    let newProduct = new Product(req.body)
+    newProduct.save(function(err) {
+        res.render('index', {title:err})
     })
 }
--- routes/product.js
+++ routes/product.js
@@ -7,4 +7,22 @@ const product_controller = require('../controllers/product')
 router.get('/count', product_controller.count)
 router.get('/testview', product_controller.testview)
 
+// CRUD
+//CREATE
+// router.get('/newProduct', render new product form)
+router.post('/', product_controller.create)
+
+//READ
+// router.get('/', product_controller.list_all)
+router.get('/:productName', product_controller.view_one)
+
+//UPDATE
+// router.post('/:productName', product_controller.update_one)
+// router.put('/:productName', update controller)
+
+//DELETE
+// router.delete('/:productName', product_controller.delete_one)
+
+
+
 module.exports = router;
