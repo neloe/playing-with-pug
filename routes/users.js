5fa15cbb913ec8b33c78188f1a2634057098676f
Nathan Eloe <powerofazure@gmail.com>
2020-04-27 10:34:46 -0500

    Add user registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+userController = require('../controllers/userController')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/register', function(req, res, next) {
+  res.render('registration', {title: 'New User'})
+})
+
+router.post('/register', userController.register)
 
 module.exports = router;
